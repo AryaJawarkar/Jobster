4228389f5696622613297cd1edba8ced5177527d
Arya Jawarkar <aryajawarkar7@gmail.com>
2023-03-02 18:12:50 +0530

    commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+import main from '../assets/images/main.svg'
+import styled from 'styled-components'
+
+
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="logo" className='logo' />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+        <h1>job <span>tracking</span> app</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos laborum molestias distinctio repellat voluptatibus iusto facere, officiis adipisci amet non.</p>
+        <button type='button' className="btn btn-hero">Login/Register</button>
+        </div>
+<img src={main} alt="main logo" className='main-img'/>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.main`
+nav{
+    width:var(--fluid-width);
+    max-width:var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display:flex;
+    align-items: center;
+}
+.page{
+    min-height:calc(100% - var(--nav-height));
+    display:grid;
+    align-items: center;
+margin-top: -2rem;
+}
+h1{
+    font-weight: 700;
+span{
+    color:var(--primary-500);
+}
+}
+p{
+    color:var(--grey-600);
+}
+.main-img{
+    display:none;
+}
+@media (min-width:992px){
+    .page{
+        grid-template-columns: 1fr 1fr;
+        column-gap : 3rem;
+    }
+    .main-img{
+    display:block;
+}
+}
+`
+export default Landing
